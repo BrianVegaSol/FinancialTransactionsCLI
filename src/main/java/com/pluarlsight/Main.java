09bd6082ef7168a6d0a848df139eec538f6a9580
BrianVegaSol <brivegasol@gmail.com>
2024-10-11 15:29:35 -0700

    Created Ledger Screen, not working
    Only loops on Ledger, doesnt return to Home Menu
    Maybe need to make Home Screen a method that Ledger calls?

--- src/main/java/com/pluarlsight/Main.java
+++ src/main/java/com/pluarlsight/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
     static Scanner scan = new Scanner(System.in);
     static boolean runHomeScreen = true;
-    static boolean runLedgerScreen = false;
+    static boolean runLedgerScreen = true;
+
     public static void main(String[] args) {
         //Welcome Message
         System.out.println("Welcome to the Financial Transaction CLI!\n" +
@@ -19,30 +20,63 @@ public class Main {
         String input = scan.nextLine();
         //Menu Options
         while (runHomeScreen) {
-        switch (Character.toUpperCase(input.charAt(0))) {
-            case 'X':
-                System.out.println("Goodbye!");
-                runHomeScreen = false;
-                break;
-            case 'D':
-                //method
-                break;
-            case 'P':
-                //method
-                break;
-            case 'L':
-                //method
-                break;
-            default:
-                System.out.println("Invalid input, try again");
-        }
+            switch (Character.toUpperCase(input.charAt(0))) {
+                case 'X':
+                    System.out.println("Goodbye!");
+                    runHomeScreen = false;
+                    break;
+                case 'D':
+                    //method
+                    break;
+                case 'P':
+                    //method
+                    break;
+                case 'L':
+                    ledger();
+                    scan.nextLine();//???
+                    runHomeScreen = true;
+                    break;
+                default:
+                    System.out.println("Invalid input, try again");
+            }
 
         }
 
-
-
+        scan.close();
     }
-    public void Ledger () {
-        runLedgerScreen = true;
+
+    public static void ledger() {
+        runHomeScreen = false;
+        //runLedgerScreen = true; //May need to be after sout
+        System.out.println("Welcome to the Ledger Menu\n" +
+                "\nInput one of the following to get started!\n" +
+                "A) All\n" +
+                "D) Deposits\n" +
+                "P) Payments\n" +
+                "R) Reports");
+        String input = scan.nextLine();
+        //Menu Options
+        while (runLedgerScreen) {
+            switch (Character.toUpperCase(input.charAt(0))) {
+                case 'H': //Home Screen
+                    runLedgerScreen = false;
+                    return;
+                    //runHomeScreen = true;
+                    //break;
+                case 'D':
+                    //method
+                    break;
+                case 'P':
+                    //method
+                    break;
+                case 'L':
+                    //method
+                    break;
+                default:
+                    System.out.println("Invalid input, try again");
+            }
+
+        }
+        //run if else for HomeMenu method???
     }
 }
