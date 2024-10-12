ef89f7facc4cd6b9c6e0299f34dcc7bb6bbf278d
BrianVegaSol <brivegasol@gmail.com>
2024-10-11 22:14:45 -0700

    "Recovery" from Failed Branch involving while loop, added switch to reports()
    This is all a mess...
    May need to revert again...
    
    Maybe make all scans into 1 big static scanner var??? might fix the while loop infinites

--- src/main/java/com/pluarlsight/Main.java
+++ src/main/java/com/pluarlsight/Main.java
@@ -139,27 +139,60 @@ public class Main {
 
 
     public static void reports () {
+        runReportsMenu = true;
         //boolean runReportsMenu = true; //need to test if this is necessary to turn off // Probably need it :)
         //May need to make this static??? Might as well
         System.out.println("Welcome to the Reports Menu\n" +
                 "\nInput one of the following to start searching!\n" +
-                "1) All\n" +
-                "2) Deposits\n" +
-                "3) Payments\n" +
-                "4) Reports\n" +
-                "5) Home\n" +
+                "1) Month To Date\n" +
+                "2) Previous Month\n" +
+                "3) Year To Date\n" +
+                "4) Previous Year\n" +
+                "5) Search By Vendor\n" +
                 "0) Back\n" +
                 "H) Home");
         //Integer.parseInt?   if hasNext else?
         while (runReportsMenu) {
-            if (scan.hasNextLine()) {
-                String reportsInput = scan.nextLine();
-                if (Character.toUpperCase(reportsInput.charAt(0)) == 'H') {
+            //if (scan.hasNextLine()) {//probably causing an infinite // still 8
+                String reportStringInput = scan.nextLine();
+                if (Character.toUpperCase(reportStringInput.charAt(0)) == 'H') {
                     runReportsMenu = false;
                     runLedgerScreen = false;
                     runHomeScreen = false;
+                } else {
+                    //int reportIntInput = scan.nextInt();
+                    //do I care do a while xO
+                    switch (Integer.parseInt(reportStringInput)) { //Might cause issues by not saving scan to var like while loop xO
+                        case 0:
+                            runReportsMenu = false;
+                            runLedgerScreen = true;
+                            break;
+                        case 1:
+                            //method
+                            //may need to turn a menu on/off
+                            break;
+                        case 2:
+                            //method
+                            //may need to turn a menu on/off
+                            break;
+                        case 3:
+                            //method
+                            //may need to turn a menu on/off
+                            break;
+                        case 4:
+                            //method
+                            //may need to turn a menu on/off
+                            break;
+                        case 5:
+                            //method
+                            //may need to turn a menu on/off
+                            break;
+                        default:
+                            System.out.println("Invalid input, try again");
+                            reportStringInput = scan.nextLine();
+                    }
                 }
-            }
+            //}
         }
 
 
